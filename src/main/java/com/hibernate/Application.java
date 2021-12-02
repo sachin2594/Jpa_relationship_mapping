2fcd89961353666927c428143584569f24636e14
sachinjadhav038@gmail.com <sachinjadhav038@gmail.com>
2021-12-02 09:30:34 +0530

    One-to-many relationship mapping between customer and product

--- src/main/java/com/hibernate/Application.java
+++ src/main/java/com/hibernate/Application.java
@@ -1,14 +1,7 @@
 package com.hibernate;
 
-import com.hibernate.entity.Course;
-import com.hibernate.repository.CourseRepository;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class Application {
